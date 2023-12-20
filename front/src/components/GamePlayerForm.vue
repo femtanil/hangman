6ffb8135e9b7a1a58ffc56477da38597b1bcc948
femtanil <femtanil@gmail.com>
2023-12-20 21:10:05 +0100

    update

--- front/src/components/GamePlayerForm.vue
+++ front/src/components/GamePlayerForm.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="absolute left-1/2 -translate-x-1/2 top-2/3 -translate-2/3">
-        <h3 class="flex justify-center text-4xl xs:text-5xl pb-9"><slot name="formTitle"></slot></h3>
-        <form  method="post">
+        <h3 class="flex justify-center text-4xl xs:text-5xl pb-9">
+            <slot name="formTitle"></slot>
+        </h3>
+        <form @submit.prevent="submitForm" method="post">
             <div class="grid grid-flow-row">
                 <label for="username" class="text-3xl xs:text-4xl">Player name</label>
-                <input type="text" id="username" name="username" class="text-3xl xs:text-4xl" />
+                <input type="text" id="username" name="username" v-model="username" class="text-3xl xs:text-4xl" />
                 <label for="password" class="text-3xl xs:text-4xl">Password</label>
-                <input type="password" id="password" name="password" class="text-3xl xs:text-4xl" />
+                <input type="password" id="password" name="password" v-model="password" class="text-3xl xs:text-4xl" />
                 <input type="submit" value="Start"
                     class=" btn btn-ghost rounded-none text-3xl xs:text-4xl active:bg-transparent w-full" />
                 <!-- Very unsafe, but it's just a demo -->
@@ -16,5 +18,38 @@
     </div>
 </template>
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { useGameStore } from '@/stores/game.js';
 import SelectScreenButton from '@/components/AppSelectScreenButton.vue';
+
+const gameStore = useGameStore();
+const username = ref('')
+const password = ref('')
+
+async function submitForm() {
+    if (gameStore.newGame == true) {
+        try {
+            await axios.post(`${import.meta.env.BACKEND_URL}/players/`, {
+                username: username.value,
+                password: password.value
+            })
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    else if (gameStore.loadGame == true) {
+        try {
+            await axios.post(`${process.env.BACKEND_URL}/token/`, {
+                username: username.value,
+                password: password.value
+            })
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
 </script>
