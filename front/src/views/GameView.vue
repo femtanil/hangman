b77350c12c40b0e5a3dcc71b53be4be3ae7ea05f
femtanil <femtanil@gmail.com>
2024-01-01 16:29:34 +0100

    Added debugging outlines

--- front/src/views/GameView.vue
+++ front/src/views/GameView.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="flex flex-col justify-between h-full max-w-full lg:grid lg:grid-cols-12">
-        <div class="collapse collapse-arrow bg-base-100 rounded-none lg:col-start-1 lg:col-end-4">
+        <div
+            class="collapse collapse-arrow bg-base-100 rounded-none lg:col-start-1 lg:col-end-4 outline outline-yellow-500">
             <input type="checkbox" />
-            <div class="collapse-title text-xl">
+            <div class="collapse-title text-xl text-indigo-500 outline outline-indigo-500">
                 first section title
             </div>
-            <div class="collapse-content bg-base-200">
+            <div class="collapse-content bg-base-200 text-rose-500 outline outline-rose-500">
                 first section content
             </div>
         </div>
-        <component :is="visibleComponent" v-bind="currentProps" />
-        <div class="collapse collapse-arrow bg-base-100 rounded-none lg:col-start-10 lg:col-end-13">
+        <component :is="visibleComponent" v-bind="currentProperties" />
+        <div
+            class="collapse collapse-arrow bg-base-100 rounded-none lg:col-start-10 lg:col-end-13 outline outline-yellow-500">
             <input type="checkbox" />
-            <div class="collapse-title text-xl">
+            <div class="collapse-title text-xl text-indigo-500 outline outline-indigo-500">
                 third section title
             </div>
-            <div class="collapse-content bg-base-200">
+            <div class="collapse-content bg-base-200 outline text-rose-500 outline-rose-500">
                 third section content
             </div>
         </div>
@@ -25,48 +27,36 @@
 import { useGameStore } from '@/stores/game.js';
 import { useAuthenticationStore } from '@/stores/authentication.js'
 import { useElementPropertiesStore } from '@/stores/elementProperties.js';
-import { ref, computed } from 'vue';
+import { computed } from 'vue';
 import SelectScreen from '@/views/GameSelectScreenView.vue';
 import MainView from '@/views/GameMainView.vue';
 
 const gameStore = useGameStore();
 const authStore = useAuthenticationStore();
 const elementPropertiesStore = useElementPropertiesStore();
-const currentProps = computed(() => {
+const currentProperties = computed(() => {
     if (gameStore.playChoice) {
         return {
             //class: `lg:col-start-4 lg:col-end-10 h-[calc(100vh-${elementPropertiesStore.statusbarHeight}px)]`
-            is: MainView,
-            class: `lg:col-start-4 lg:col-end-10`
+            class: `lg:col-start-4 lg:col-end-10 h-full`
         };
     }
     else if (authStore.tokenData !== null && gameStore.player !== null) {
         return {
-            is: MainView,
             class: `lg:col-start-4 lg:col-end-10`
         };
     }
     else {
         return {
-            is: SelectScreen,
-            class: 'lg:col-start-4 lg:col-end-10'
         };
     }
 });
 
 const visibleComponent = computed(() => {
     if (gameStore.playChoice) {
-        currentProps.value = {
-            //class: `lg:col-start-4 lg:col-end-10 h-[calc(100vh-${elementPropertiesStore.statusbarHeight}px)]`
-            class: `lg:col-start-4 lg:col-end-10`
-        };
-        console.log(elementPropertiesStore.statusbarHeight);
         return MainView;
     }
     else if (authStore.tokenData !== null && gameStore.player !== null) {
-        currentProps.value = {
-            class: `lg:col-start-4 lg:col-end-10`
-        };
         return MainView;
     }
     else {
