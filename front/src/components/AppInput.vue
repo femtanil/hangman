c646b489b25c322b676c9838d0699a6c600ee0a6
femtanil <femtanil@gmail.com>
2024-01-04 17:16:09 +0100

    Added form input component

---
+++ front/src/components/AppInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <label v-if="label">{{ label }}</label>
+    <input type="text" :value="modelValue" @input="emit('update:modelValue', $event.target.value)" v-bind="$attrs" />
+</template>
+<script setup>
+const props = defineProps({
+    label: {
+        type: [String, Boolean],
+    },
+    modelValue: {
+        type: String,
+        default: '',
+    }
+})
+
+const emit = defineEmits(['update:modelValue']);
+</script>
