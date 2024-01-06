416a0f76948880c29d9ca6dadae54b3b665c0649
femtanil <femtanil@gmail.com>
2024-01-06 19:13:10 +0100

    Updated submit behaviour and input components

--- front/src/components/GameAuthenticationForm.vue
+++ front/src/components/GameAuthenticationForm.vue
@@ -3,25 +3,23 @@
         <h3 class="flex justify-center text-2xl">
             <slot name="formTitle"></slot>
         </h3>
-        <form @submit.prevent="submitForm" method="post">
+        <form @submit="onSubmit" method="post">
             <div class="flex flex-col px-5">
-                <AppInput v-model="username" v-bind="usernameAttrs" type="text" class="input input-bordered text-xl"
-                    :label="'Username'" />
+                <AppInput v-model="username" v-bind="usernameAttrs" :label="'Username'" type="text" />
             </div>
             <div class="flex flex-col px-5">
-                <AppInput v-model="password" type="password" v-bind="passwordAttrs" class="input input-bordered text-xl"
-                    :label="'Password'" />
+                <AppInput v-model="password" v-bind="passwordAttrs" :label="'Password'" type="password" />
             </div>
             <div v-if="props.confirmPassword" class="flex flex-col px-5">
-                <AppInput v-model="passwordConfirmation" v-bind="passwordConfirmationAttrs" type="password"
-                    class="input input-bordered text-xl" :label="'Confirm password'" />
+                <AppInput v-model="passwordConfirmation" v-bind="passwordConfirmationAttrs" :label="'Confirm password'"
+                    type="password" />
             </div>
             <div class="flex justify-center">
-                <AppInput type="submit" value="Login" class="btn btn-ghost rounded-none text-3xl active:bg-transparent" />
+                <AppButton :disabled="isSubmitting" type="submit">{{ 'Login' }}</AppButton>
             </div>
             <div class="flex justify-center">
                 <div class="grid grid-flow-row">
-                    <SelectScreenButton />
+                    <SelectScreenButton :disabled="isSubmitting" />
                 </div>
             </div>
         </form>
@@ -33,6 +31,7 @@ import { useForm } from 'vee-validate';
 import { toTypedSchema } from '@vee-validate/yup';
 import { object, string } from 'yup';
 import AppInput from '@/components/AppInput.vue';
+import AppButton from '@/components/AppButton.vue';
 import SelectScreenButton from '@/components/AppSelectScreenButton.vue';
 
 const authenticationStore = useAuthenticationStore();
@@ -45,26 +44,28 @@ const props = defineProps({
 
 const schema = toTypedSchema(
     object({
-        username: string().required().min(3).max(20),
-        password: string().required().min(1).max(20),
-        passwordConfirmation: string().required().min(8).max(20)
+        username: string().required().min(3).max(20).default(''),
+        password: string().required().min(1).max(20).default(''),
+        //passwordConfirmation: string().required().min(8).max(20)
     }),
 );
-const { values, errors, defineField } = useForm({
+const { errors, handleSubmit, isSubmitting, defineField } = useForm({
     validationSchema: schema,
 });
 
 const [username, usernameAttrs] = defineField('username');
 const [password, passwordAttrs] = defineField('password');
-const [passwordConfirmation, passwordConfirmationAttrs] = defineField('passwordConfirmation');
+//const [passwordConfirmation, passwordConfirmationAttrs] = defineField('passwordConfirmation');
 
-async function submitForm() {
+const onSubmit = handleSubmit(async (values, { resetForm }) => {
     const data = new FormData();
     data.append('username', values.username);
     data.append('password', values.password);
     data.append('scope',
         'user.create user:own user:own.write user:own:player user:own:player.write user:others:player:points user:others:player:playername');
-    authenticationStore.loginUser(data);
-}
+    const response = await authenticationStore.loginUser(data);
+    resetForm();
+    return response;
+});
 
 </script>
