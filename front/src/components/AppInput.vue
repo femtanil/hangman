fa30a0db4e2f7742367094d1dff7fe9834fbf4c3
femtanil <femtanil@gmail.com>
2024-01-06 19:11:56 +0100

    Updated AppInput to be more reusable

--- front/src/components/AppInput.vue
+++ front/src/components/AppInput.vue
@@ -1,15 +1,29 @@
 <template>
-    <label v-if="label">{{ label }}</label>
-    <input type="text" :value="modelValue" @input="emit('update:modelValue', $event.target.value)" v-bind="$attrs" />
+    <label v-if="label" class="label-text">{{ label }}</label>
+    <input :value="modelValue" @input="emit('update:modelValue', $event.target.value)" v-bind="{ ...$attrs, ...classes }"/>
 </template>
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
     label: {
+        type: [String, Boolean],
+    },
+    class: {
         type: [String, Boolean],
     },
     modelValue: {
         type: String,
         default: '',
+    }
+})
+
+const classes = computed(() => {
+    if (props.class) {
+        return { class: props.class };
+    }
+    else {
+        return { class: 'input input-bordered text-xl' };
     }
 })
 
